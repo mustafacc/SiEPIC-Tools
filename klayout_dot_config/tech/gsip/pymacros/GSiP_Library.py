c8aa4b5b460471f369065e60b6c25b2bab956c1a
mustafacc <mustafa@siepic.com>
2024-01-08 05:00:35 -0800

    setup gsip to be an independently importable package

--- klayout_dot_config/tech/gsip/pymacros/GSiP_Library.py
+++ klayout_dot_config/tech/gsip/pymacros/GSiP_Library.py
@@ -1,36 +1,6 @@
 """
 This file is part of the SiEPIC-Tools and SiEPIC-GSiP PDK
-by Lukas Chrostowski (c) 2015-2017
-
-This Python file implements a library called "GSiP" for scripted and GUI-based layout flows. 
-
-Crash warning:
- https://www.klayout.de/forum/comments.php?DiscussionID=734&amp;page=1#Item_13
- This library has nested PCells. Running this macro with a layout open may
- cause it to crash. Close the layout first before running.
-
-Version history:
-
-Mustafa Hammood 2020/6/25
-- Refactored PCells out of library files into individual files in a subdirectory
-
-Jaspreet Jhoja 2020/5/23
-- Refactored PCells to make them compatible with both, GUI and script-based layout operations
-
-Stefan Preble and Karl McNulty (RIT) 2019/6/13
- - Wireguide : Path to metal wires 
-
-Lukas Chrostowski 2017/12/16
- - compatibility with KLayout 0.25 and SiEPIC-Tools
-
-Lukas Chrostowski
- - GDS cells (detector, etc) and PCells (ring modulator, filter)
- 
-todo:
-replace:     
- layout_arc_wg_dbu(self.cell, Layerm1N, x0,y0, r_m1_in, w_m1_in, angle_min_doping, angle_max_doping)
-with:
- self.cell.shapes(Layerm1N).insert(pya.Polygon(arc(w_m1_in, angle_min_doping, angle_max_doping) + [pya.Point(0, 0)]).transformed(t))
+Lukas Chrostowski (c) 2015-2024
 """
 
 folder = 'pcells_GSiP'
