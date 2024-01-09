134bc100f722f3ffdbb9e2aa4ca4797e73d160a1
mustafacc <mustafa@siepic.com>
2024-01-09 04:54:34 -0800

    reformat examples

--- examples/script/00_layout/00_layout.py
+++ examples/script/00_layout/00_layout.py
@@ -4,11 +4,12 @@ import gsip
 from SiEPIC.utils.layout import new_layout, floorplan
 from SiEPIC.scripts import export_layout
 
-# %% create a new layout with empty floorplan
-topcell, ly = new_layout(tech=gsip.name, topcell_name="TOP", overwrite=True)
-floorplan(topcell, 605e3, 410e3)
+if __name__ == '__main__':
+    # create an empty layout with only a floorplan
+    topcell, ly = new_layout(tech=gsip.name, topcell_name="TOP", overwrite=True)
+    floorplan(topcell, 605e3, 410e3)
 
-# %% export the layout
-fpath = os.path.dirname(os.path.abspath(__file__))
-fname = os.path.basename(fpath).removesuffix(".py")
-export_layout(topcell, fpath, fname, format="oas", screenshot=True)
+    # export the layout
+    fpath = os.path.dirname(os.path.abspath(__file__))
+    fname = os.path.basename(fpath).removesuffix(".py")
+    export_layout(topcell, fpath, fname, format="oas", screenshot=True)
--- examples/script/02_routing/02_routing.py
+++ examples/script/02_routing/02_routing.py
@@ -6,37 +6,40 @@ from SiEPIC.scripts import export_layout, connect_pins_with_waveguide
 from SiEPIC.extend import to_itype
 from pya import Trans, CellInstArray, Text
 
-# %% create a new layout with back-to-back GC
-user = "Mustafa"
-gc_cell = "Grating_Coupler_13deg_TE_1550_Oxide"
-waveguide = "Strip TE 1550 nm"
+# create a new layout with back-to-back GC
+if __name__ == '__main__':
+    user = "Mustafa"
+    gc_cell = "Grating_Coupler_13deg_TE_1550_Oxide"
+    waveguide = "Strip TE 1550 nm"
 
-topcell, ly = new_layout(tech=gsip.name, topcell_name="TOP", overwrite=True)
-floorplan(topcell, to_itype(605), to_itype(410))
+    # define a new layout with empty cell
+    topcell, ly = new_layout(tech=gsip.name, topcell_name="TOP", overwrite=True)
+    floorplan(topcell, to_itype(605), to_itype(410))
 
-cell_gc = ly.create_cell(gc_cell, gsip.name)
-x = to_itype(100)
-y = to_itype(100)
+    # create a gc cell
+    cell_gc = ly.create_cell(gc_cell, gsip.name)
 
-t = Trans(Trans.R0, x, y)
-instGC1 = topcell.insert(CellInstArray(cell_gc.cell_index(), t))
+    # create the 1st GC instance
+    x = to_itype(100)
+    y = to_itype(100)
+    t = Trans(Trans.R0, x, y)
+    instGC1 = topcell.insert(CellInstArray(cell_gc.cell_index(), t))
 
-y += to_itype(127)
-t = Trans(Trans.R0, x, y)
-instGC2 = topcell.insert(CellInstArray(cell_gc.cell_index(), t))
+    # create the 2nd GC instance, 127 microns above the 1st
+    y += to_itype(127)
+    t = Trans(Trans.R0, x, y)
+    instGC2 = topcell.insert(CellInstArray(cell_gc.cell_index(), t))
 
-# automated test label
-text = Text(f"opt_in_TE_1550_device_{user}_MZI1", t)
-topcell.shapes(ly.layer(ly.TECHNOLOGY["Text"])).insert(text).text_size = 2 / ly.dbu
+    # automated test label
+    text = Text(f"opt_in_TE_1550_device_{user}_MZI1", t)
+    topcell.shapes(ly.layer(ly.TECHNOLOGY["Text"])).insert(text).text_size = 2 / ly.dbu
 
-# connect GCs
-connect_pins_with_waveguide(
-    instGC1, "opt_wg", instGC2, "opt_wg", waveguide_type=waveguide
-)
+    # connect GCs by a waveguide
+    connect_pins_with_waveguide(
+        instGC1, "opt_wg", instGC2, "opt_wg", waveguide_type=waveguide
+    )
 
-# %% export the layout
-fpath = os.path.dirname(os.path.abspath(__file__))
-fname = os.path.basename(fpath).removesuffix(".py")
-export_layout(topcell, fpath, fname, format="oas", screenshot=True)
-
-# %%
+    # export the layout
+    fpath = os.path.dirname(os.path.abspath(__file__))
+    fname = os.path.basename(fpath).removesuffix(".py")
+    export_layout(topcell, fpath, fname, format="oas", screenshot=True)
