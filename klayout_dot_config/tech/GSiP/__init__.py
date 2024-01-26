242d62132a13d4fea5dae24229135dbb2734f3be
mustafacc <mustafa@siepic.com>
2024-01-26 13:32:34 -0800

    change library loading to not use replicated code

--- klayout_dot_config/tech/GSiP/__init__.py
+++ klayout_dot_config/tech/GSiP/__init__.py
@@ -1,11 +1,16 @@
-print('SiEPIC-GSiP PDK Python module: siepic_gsip_pdk, KLayout technology: GSipP')
-
 # Load the KLayout technology, when running in Script mode
+from SiEPIC.utils import get_technology_by_name
 import pya, os
-tech = pya.Technology().create_technology('GSiP')
-tech.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'GSiP.lyt'))
 
+name = "GSiP"
+
+print(f"SiEPIC PDK Python module: KLayout technology: {name}")
+
+try:
+    tech = pya.Technology().create_technology(name)
+    tech.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), f"{name}.lyt"))
+except Exception as e:
+    tech = get_technology_by_name(name)
+    print(f"Technology {name} already loaded.")
 # then import all the technology modules
 from . import pymacros
-
-
